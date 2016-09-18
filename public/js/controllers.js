2ba9ad57937e9e5136e89bbb2248f65e59346bdd
Lisa <misslisazhou@gmail.com>
2016-09-17 15:30:23 -1000

    step12_service

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,10 @@
 angular.module('myApp')
-  .controller('myController', ['$scope', 'mainCharacter', 'CharacterVersionFactory', function ($scope, mainCharacter, CharacterVersionFactory) {
+  .controller('myController', ['$scope', 'mainCharacter', 'CharacterVersionFactory', 'BookService', function ($scope, mainCharacter, CharacterVersionFactory, BookService) {
+    console.log(BookService.getBooks());
+    console.log(BookService.getBook(2));
     $scope.myFirstName = 'Lisa';
     $scope.myModel = 'Real Player One';
     $scope.mainCharacter = mainCharacter;
     $scope.characterVersion = CharacterVersionFactory;
+    $scope.books = BookService.getBooks();
   }])
---
+++ public/js/services.js
@@ -0,0 +1,31 @@
+angular.module('myApp')
+  .service('BookService', BookService);
+
+function BookService() {
+  this.value = "I am a Book Service!";
+  this.books = [
+    {
+      id: 1,
+      title: 'Green Eggs and Ham',
+      author: 'Dr. Suess'
+    },
+    {
+      id: 2,
+      title: 'Harry Potter',
+      author: 'J.K. Rowling'
+    },
+    {
+      id: 3,
+      title: 'The Giving Tree',
+      author: 'Shel Silverstein'
+    },
+  ]
+}
+
+BookService.prototype.getBooks = function () {
+  return this.books
+}
+
+BookService.prototype.getBook = function (id) {
+  return this.books.find((book) => book.id === id)
+}
