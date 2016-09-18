d88ca4925846fd7f16698299a863d9e5569a67cd
Lisa <misslisazhou@gmail.com>
2016-09-17 18:11:48 -1000

    step15_provider

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,9 +1,14 @@
 angular.module('myApp')
-  .controller('myController', ['$scope', 'mainCharacter', 'CharacterVersionFactory', 'BookService', function ($scope, mainCharacter, CharacterVersionFactory, BookService) {
+  .controller('myController', ['$scope', 'mainCharacter', 'CharacterVersionFactory', 'BookService', 'Movies', function ($scope, mainCharacter, CharacterVersionFactory, BookService, Movies) {
     $scope.myFirstName = 'Lisa';
     $scope.myModel = 'Real Player One';
     $scope.mainCharacter = mainCharacter;
     $scope.characterVersion = CharacterVersionFactory;
     // $scope.books = BookService.getBooks();
     $scope.BookService = BookService;
+    $scope.movies = [];
+    Movies.getMovies()
+      .then(function (response) {
+        $scope.movies = response.data;
+      })
   }])
--- server.js
+++ server.js
@@ -2,9 +2,44 @@ var express = require('express');
 var app = express();
 
 var PORT = 8080;
-
+var movies = [
+  {
+    id: 1,
+    title: 'Suicide Squad',
+    year: 2016
+  },
+  {
+    id: 2,
+    title: 'The Dark Knight',
+    year: 2008
+  },
+  {
+    id: 3,
+    title: 'Willy Wonka & the Chocolate Factory',
+    year: 1971
+  },
+  {
+    id: 4,
+    title: 'Star Wars: A New Hope',
+    year: 1977
+  },
+  {
+    id: 5,
+    title: 'Star Wars: The Empire Strikes Back',
+    year: 1980
+  },
+  {
+    id: 6,
+    title: 'Star Wars: Return of the Jedi',
+    year: 1983
+  }
+]
 app.use(express.static('public'));
 
+
+app.get('/api/movies', function (req, res) {
+  res.json(movies);
+})
 app.get('/', function (req, res) {
   res.render('index.html')
 })
